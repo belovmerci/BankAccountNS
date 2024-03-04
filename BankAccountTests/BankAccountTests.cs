08b05c37238731c9a4c2f696475229f52a95fb63
BelovAA <BelovAA@206D-4>
2024-03-04 11:15:31 +0700

    Добавьте файлы проекта.

---
+++ BankAccountTests/BankAccountTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankAccountNS.Tests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            BankAccount account = new BankAccount("Mr. Test", 100.00);
+
+            // Act
+            account.Debit(25.50);
+
+            // Assert
+            Assert.AreEqual(74.50, account.Balance, 0.001, "Количество денег на счёте не было обновлено корректно.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WithNegativeAmount_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            BankAccount account = new BankAccount("Mr. Test", 100.00);
+
+            // Act
+            account.Debit(-5.00);
+
+            // Assert
+            // Expecting an ArgumentOutOfRangeException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_ExceedsBalance_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            BankAccount account = new BankAccount("Mr. Test", 50.00);
+
+            // Act
+            account.Debit(75.00);
+
+            // Assert
+            // Expecting an ArgumentOutOfRangeException
+        }
+
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            BankAccount account = new BankAccount("Mr. Test", 100.00);
+
+            // Act
+            account.Credit(30.50);
+
+            // Assert
+            Assert.AreEqual(130.50, account.Balance, 0.001, "Количество кред. линии не было обновлено корректно.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Credit_WithNegativeAmount_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            BankAccount account = new BankAccount("Mr. Test", 100.00);
+
+            // Act
+            account.Credit(-5.00);
+
+            // Assert
+            // Expecting an ArgumentOutOfRangeException
+        }
+
+        [TestMethod]
+        public void Main_Method_CorrectlyUpdatesBalance()
+        {
+            // Arrange
+            BankAccount account = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            // Act
+            account.Credit(5.77);
+            account.Debit(11.22);
+
+            // Assert
+            Assert.AreEqual(6.54, account.Balance, 0.001, "Главный метод не обновил баланс корректно.");
+        }
+    }
+}
+
---
+++ BankAccountTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankAccountTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee713e98-c894-4e46-87bc-2304f5aa3496")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
